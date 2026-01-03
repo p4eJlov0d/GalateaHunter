cbdcce86471418f5e2561cae71f75fb19ff1fb9f
p4eJlov0d <bkmz1d@inbox.ru>
2026-01-04 01:32:12 +0500

    several elements have been added to the main screen

--- src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
@@ -1,8 +1,12 @@
 package ru.p4ejlov0d.galateahunter.client;
 
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+
+import static net.minecraft.text.Text.translatable;
 
 public class GalateaHunterScreen extends Screen {
 
@@ -14,5 +18,17 @@ public class GalateaHunterScreen extends Screen {
 
     public GalateaHunterScreen() {
         super(TITLE);
+    }
+
+    @Override
+    protected void init() {
+        TextWidget textWidget = new TextWidget(10, 10, 120, 20, translatable("galateahunter.lang"), textRenderer);
+        ButtonWidget buttonWidget = ButtonWidget.builder(translatable("galateahunter.btn"), (btn) -> {
+
+        }).dimensions(40, 40, 120, 20).build();
+
+        addDrawableChild(textWidget);
+        addDrawableChild(buttonWidget);
+
     }
 }
