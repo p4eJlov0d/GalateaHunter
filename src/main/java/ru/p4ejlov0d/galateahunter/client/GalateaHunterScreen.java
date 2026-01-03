401faa54a199211c4af9222afbfa3ab0fd3288f0
p4eJlov0d <bkmz1d@inbox.ru>
2026-01-04 03:45:52 +0500

    added a loader for mod language resources

--- src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
@@ -3,17 +3,23 @@ package ru.p4ejlov0d.galateahunter.client;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextWidget;
+import net.minecraft.resource.Resource;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static net.minecraft.text.Text.translatable;
 
 public class GalateaHunterScreen extends Screen {
 
+    public static final Map<String, Resource> LANG_FILES;
     private static final MutableText TITLE;
 
     static {
         TITLE = Text.literal("Galatea Hunter");
+        LANG_FILES = new LinkedHashMap<>();
     }
 
     public GalateaHunterScreen() {
---
+++ src/main/java/ru/p4ejlov0d/galateahunter/handler/LanguageResourceReloadListener.java
@@ -0,0 +1,24 @@
+package ru.p4ejlov0d.galateahunter.handler;
+
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import ru.p4ejlov0d.galateahunter.client.GalateaHunterScreen;
+
+import static ru.p4ejlov0d.galateahunter.GalateaHunter.MOD_ID;
+
+public class LanguageResourceReloadListener implements SimpleSynchronousResourceReloadListener {
+    @Override
+    public Identifier getFabricId() {
+        return Identifier.tryParse(MOD_ID, "load_lang");
+    }
+
+    @Override
+    public void reload(ResourceManager manager) {
+        for (Identifier id : manager.findResources("lang", path -> path.toString().endsWith(".json")).keySet()) {
+            if (id.getNamespace().equals(MOD_ID)) {
+                GalateaHunterScreen.LANG_FILES.put(id.getPath().split("/")[1].split("\\.")[0], manager.getResource(id).get());
+            }
+        }
+    }
+}
