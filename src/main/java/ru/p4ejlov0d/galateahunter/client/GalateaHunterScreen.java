f453ffe49c326b0dc70ff3874691d2cd6ff4f762
p4eJlov0d <bkmz1d@inbox.ru>
2026-01-06 19:16:44 +0500

    tracking list has been added to the hunting category

--- src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
@@ -3,12 +3,17 @@ package ru.p4ejlov0d.galateahunter.client;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
 import me.shedaniel.clothconfig2.gui.entries.SelectionListEntry;
+import me.shedaniel.clothconfig2.gui.entries.SubCategoryListEntry;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Colors;
 import ru.p4ejlov0d.galateahunter.model.LanguageModel;
 import ru.p4ejlov0d.galateahunter.utils.LanguageResourceHandler;
+
+import java.util.List;
 
 import static ru.p4ejlov0d.galateahunter.GalateaHunter.*;
 
@@ -70,6 +75,24 @@ public class GalateaHunterScreen {
 
         ConfigCategory hunting = configBuilder.getOrCreateCategory(Text.literal(languageModel.huntingCategory()));
         hunting.setDescription(LanguageModel.parseTexts(languageModel.huntingDescriptions()));
+
+        BooleanListEntry huntingBoxToggle = entryBuilder.startBooleanToggle(Text.literal(languageModel.huntingBox()), true)
+                .setDefaultValue(true)
+                .setYesNoTextSupplier(bool -> bool ? Text.literal(languageModel.enabled()).withColor(Colors.GREEN) : Text.literal(languageModel.disabled()).withColor(Colors.LIGHT_RED))
+                .setTooltip(Text.literal(languageModel.huntingBoxTooltip()))
+                .build();
+
+        BooleanListEntry attributeMenuToggle = entryBuilder.startBooleanToggle(Text.literal(languageModel.attributeMenu()), true)
+                .setDefaultValue(true)
+                .setYesNoTextSupplier(bool -> bool ? Text.literal(languageModel.enabled()).withColor(Colors.GREEN) : Text.literal(languageModel.disabled()).withColor(Colors.LIGHT_RED))
+                .setTooltip(Text.literal(languageModel.attributeMenuTooltip()))
+                .build();
+
+        SubCategoryListEntry trackingSubCategory = entryBuilder.startSubCategory(Text.literal(languageModel.tracking()), List.of(huntingBoxToggle, attributeMenuToggle))
+                .setTooltip(Text.literal(languageModel.trackingTooltip()))
+                .build();
+
+        hunting.addEntry(trackingSubCategory);
 
         LOGGER.debug("Successfully created hunting category");
     }
--- src/main/java/ru/p4ejlov0d/galateahunter/model/LanguageModel.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/model/LanguageModel.java
@@ -12,7 +12,15 @@ public record LanguageModel(
         @JsonProperty("galateahunter.reset") String reset,
         @JsonProperty("galateahunter.hunting_category") String huntingCategory,
         @JsonProperty("galateahunter.general_descriptions") String[] generalDescriptions,
-        @JsonProperty("galateahunter.hunting_descriptions") String[] huntingDescriptions
+        @JsonProperty("galateahunter.hunting_descriptions") String[] huntingDescriptions,
+        @JsonProperty("galateahunter.tracking") String tracking,
+        @JsonProperty("galateahunter.tracking_tooltip") String trackingTooltip,
+        @JsonProperty("galateahunter.hunting_box") String huntingBox,
+        @JsonProperty("galateahunter.hunting_box_tooltip") String huntingBoxTooltip,
+        @JsonProperty("galateahunter.attribute_menu") String attributeMenu,
+        @JsonProperty("galateahunter.attribute_menu_tooltip") String attributeMenuTooltip,
+        @JsonProperty("galateahunter.enabled_bool") String enabled,
+        @JsonProperty("galateahunter.disabled_bool") String disabled
 ) {
     public static MutableText[] parseTexts(String[] descriptions) {
         MutableText[] texts = new MutableText[descriptions.length];
