3d6b7f3d328f470b7fc3b6d04f5ff9d06775edcc
p4eJlov0d <bkmz1d@inbox.ru>
2026-01-06 20:46:36 +0500

    added game test implementation

---
+++ src/gametest/java/ru/p4ejlov0d/galateahunter/GalateaHunterGameTest.java
@@ -0,0 +1,17 @@
+package ru.p4ejlov0d.galateahunter;
+
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
+
+@SuppressWarnings("UnstableApiUsage")
+public class GalateaHunterGameTest implements FabricClientGameTest {
+    /**
+     * Runs the gametest.
+     *
+     * @param context
+     */
+    @Override
+    public void runTest(ClientGameTestContext context) {
+
+    }
+}
