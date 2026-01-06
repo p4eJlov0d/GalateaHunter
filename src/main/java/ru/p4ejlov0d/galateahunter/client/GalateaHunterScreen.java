fae1eb085a06bb30afa466d7a1ce625d1a57f59e
p4eJlov0d <bkmz1d@inbox.ru>
2026-01-06 17:31:24 +0500

    added hunting category

--- src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/client/GalateaHunterScreen.java
@@ -5,7 +5,6 @@ import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.SelectionListEntry;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import ru.p4ejlov0d.galateahunter.model.LanguageModel;
@@ -31,25 +30,47 @@ public class GalateaHunterScreen {
         LanguageResourceHandler languageResourceHandler = LanguageResourceHandler.getInstance();
         LanguageModel languageModel = languageResourceHandler.getLanguageModel();
 
-        LOGGER.debug("Creating general GUI");
+        LOGGER.debug("Creating main mod GUI");
 
         ConfigBuilder configBuilder = ConfigBuilder.create().setParentScreen(parent).setTitle(TITLE);
         ConfigEntryBuilder entryBuilder = configBuilder.entryBuilder();
+
+        entryBuilder.setResetButtonKey(Text.literal(languageModel.reset()));
+
+        // categories
+        createGeneralCategory(configBuilder, languageModel, entryBuilder, languageResourceHandler);
+        createHuntingCategory(configBuilder, languageModel, entryBuilder);
+
+        Screen generalScreen = configBuilder.build();
+
+        LOGGER.debug("Successfully created main mod GUI");
+
+        return generalScreen;
+    }
+
+    private static void createGeneralCategory(ConfigBuilder configBuilder, LanguageModel languageModel, ConfigEntryBuilder entryBuilder, LanguageResourceHandler languageResourceHandler) {
+        LOGGER.debug("Creating general category");
+
         ConfigCategory general = configBuilder.getOrCreateCategory(Text.literal(languageModel.generalCategory()));
+        general.setDescription(LanguageModel.parseTexts(languageModel.generalDescriptions()));
 
         SelectionListEntry<String> changeLanguage = entryBuilder.startSelector(Text.literal(languageModel.lang()), languageResourceHandler.loadLangNames(), languageModel.langName())
                 .setDefaultValue("English")
                 .setSaveConsumer(languageResourceHandler::changeLangCodeByLangName)
                 .setTooltip(Text.literal(languageModel.languageTooltip()))
                 .build();
-        ((ClickableWidget) changeLanguage.children().getLast()).setMessage(Text.literal(languageModel.reset()));
 
         general.addEntry(changeLanguage);
 
-        Screen generalScreen = configBuilder.build();
+        LOGGER.debug("Successfully created general category");
+    }
 
-        LOGGER.debug("Successfully created general GUI");
+    private static void createHuntingCategory(ConfigBuilder configBuilder, LanguageModel languageModel, ConfigEntryBuilder entryBuilder) {
+        LOGGER.debug("Creating hunting category");
 
-        return generalScreen;
+        ConfigCategory hunting = configBuilder.getOrCreateCategory(Text.literal(languageModel.huntingCategory()));
+        hunting.setDescription(LanguageModel.parseTexts(languageModel.huntingDescriptions()));
+
+        LOGGER.debug("Successfully created hunting category");
     }
 }
--- src/main/java/ru/p4ejlov0d/galateahunter/model/LanguageModel.java
+++ src/main/java/ru/p4ejlov0d/galateahunter/model/LanguageModel.java
@@ -1,12 +1,26 @@
 package ru.p4ejlov0d.galateahunter.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 
 public record LanguageModel(
         @JsonProperty("galateahunter.general_category") String generalCategory,
         @JsonProperty("galateahunter.lang") String lang,
         @JsonProperty("galateahunter.lang_name") String langName,
         @JsonProperty("galateahunter.language_tooltip") String languageTooltip,
-        @JsonProperty("galateahunter.reset") String reset
+        @JsonProperty("galateahunter.reset") String reset,
+        @JsonProperty("galateahunter.hunting_category") String huntingCategory,
+        @JsonProperty("galateahunter.general_descriptions") String[] generalDescriptions,
+        @JsonProperty("galateahunter.hunting_descriptions") String[] huntingDescriptions
 ) {
+    public static MutableText[] parseTexts(String[] descriptions) {
+        MutableText[] texts = new MutableText[descriptions.length];
+
+        for (int i = 0; i < descriptions.length; i++) {
+            texts[i] = Text.literal(descriptions[i]);
+        }
+
+        return texts;
+    }
 }
